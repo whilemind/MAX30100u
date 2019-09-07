73187e303c6f4081b067cd19bfa93c17c70227f0
Mahabub <babu.coder@gmail.com>
2019-09-07 14:58:38 +1000

    1. now  it is showing BPM

--- max30100.py
+++ max30100.py
@@ -1,13 +1,6 @@
-""""
-  Library for the Maxim MAX30100 pulse oximetry system on Raspberry Pi
-
-  Based on original C library for Arduino by Connor Huffine/Kontakt
-  https: // github.com / kontakt / MAX30100
-
-  September 2017
-"""
-
 import smbus
+import math
+import time
 
 INT_STATUS   = 0x00  # Which interrupts are tripped
 INT_ENABLE   = 0x01  # Which interrupts are active
@@ -84,16 +77,59 @@ MODE_HR = 0x02
 MODE_SPO2 = 0x03
 
 
+class pulseoxymeter_t(object):
+    def __init__(self,
+        pulseDetected = False,
+        heartBPM = 0.0,
+        irCardiogram = 0.0,
+        irDcValue = 0.0,
+        redDcValue = 0.0,
+        currentSaO2Value = 0.0,
+        lastBeatThreshold = 0.0,
+        dcFilteredIR = 0.0,
+        dcFilteredRed = 0.0
+        ):
+        self.pulseDetected = pulseDetected
+        self.heartBPM = heartBPM,
+        self.irCardiogram = irCardiogram,
+        self.irDcValue = irDcValue,
+        self.redDcValue = redDcValue,
+        self.SaO2 = currentSaO2Value,
+        self.lastBeatThreshold = lastBeatThreshold,
+        self.dcFilteredIR = dcFilteredIR,
+        self.dcFilteredRed = 0.0
+
+
+class dcFilter_t(object): 
+    def __init__(self, w=0.0, result=0.0):
+        self.w = w
+        self.result = result
+
+
+class meanDiffFilter_t(object):
+    def __init__(self, index = 0, sum = 0, count = 0):
+        self.values = range(15)
+        self.index = index
+        self.sum = sum
+        self.count = count
+
+class butterworthFilter_t(object):
+    def __init__(self, result=0.0):
+        self.v = range(2)
+        self.result = result
+
+
 class MAX30100(object):
 
     def __init__(self,
                  i2c=None,
                  mode=MODE_HR,
                  sample_rate=100,
-                 led_current_red=11.0,
-                 led_current_ir=11.0,
+                 led_current_red=27.1,
+                 led_current_ir=27.1,
                  pulse_width=1600,
-                 max_buffer_len=10000
+                 max_buffer_len=10000,
+                 debug=False
                  ):
 
         # Default to the standard I2C bus on Pi.
@@ -110,12 +146,51 @@ class MAX30100(object):
         self.sample_rate = sample_rate
         self.pulse_width = pulse_width
         self.max_buffer_len = max_buffer_len
-                
+
         self.buffer_red = []
         self.buffer_ir = []
 
-        self.max_buffer_len = max_buffer_len
         self._interrupt = None
+
+
+        # Update classes for BPM OS2 calcualtion.
+        # This are enum type
+        self.PULSE_IDLE = 0
+        self.PULSE_TRACE_UP = 1
+        self.PULSE_TRACE_DOWN = 2
+
+        self.MAGIC_ACCEPTABLE_INTENSITY_DIFF = 65000
+        self.RED_LED_CURRENT_ADJUSTMENT_MS = 500
+        self.RESET_SPO2_EVERY_N_PULSES = 4
+        self.ALPHA = 0.95
+        self.MEAN_FILTER_SIZE = 15  
+
+        self.PULSE_MIN_THRESHOLD = 100 #300 is good for finger, but for wrist you need like 20, and there is shitloads of noise
+        self.PULSE_MAX_THRESHOLD = 2000
+        self.PULSE_GO_DOWN_THRESHOLD = 1
+        self.PULSE_BPM_SAMPLE_SIZE = 10 
+
+        self.debug = debug
+        self.dcFilterIR = dcFilter_t()
+        self.dcFilterRed = dcFilter_t()
+        self.meanDiffIR = meanDiffFilter_t()
+        self.meanDiffIR = meanDiffFilter_t()
+        self.lpbFilterIR = butterworthFilter_t()
+        self.irACValueSqSum = 0
+        self.redACValueSqSum = 0
+        self.samplesRecorded = 0
+        self.pulsesDetected = 0
+        self.currentSaO2Value = 0
+        self.currentPulseDetectorState = self.PULSE_IDLE
+        self.lastBeatThreshold = 0
+        self.currentBPM = 0
+        
+        self.valuesBPM = range(self.PULSE_BPM_SAMPLE_SIZE)
+        self.valuesBPMSum = 0
+        self.valuesBPMCount = 0
+        self.bpmIndex = 0
+
+
 
 
     def reinit(self):
@@ -131,6 +206,177 @@ class MAX30100(object):
 
         self.max_buffer_len = 10000
         self._interrupt = None
+
+
+    def update(self):
+        result = pulseoxymeter_t(currentSaO2Value=self.currentSaO2Value)
+        self.read_sensor()
+        
+        self.dcFilterIR = self.dcRemoval(self.ir, self.dcFilterIR.w, self.ALPHA)
+        self.dcFilterRed = self.dcRemoval(self.red, self.dcFilterRed.w, self.ALPHA)       
+
+        
+        (meanDiffResIR, self.meanDiffIR) = self.meanDiff(self.dcFilterIR.result, self.meanDiffIR)
+    
+        if(self.debug):
+            print("Ir {} and red {}".format(self.ir, self.red))
+            print("dcFilterIR w {} result {}".format(self.dcFilterIR.w, self.dcFilterIR.result))
+            print("dcFilterRed w {} result {}".format(self.dcFilterRed.w, self.dcFilterRed.result))
+            print("meanDiffResIR {} IR filter {}".format(meanDiffResIR, self.meanDiffIR.values))
+
+        self.lpbFilterIR = self.lowPassButterworthFilter( meanDiffResIR, self.lpbFilterIR )
+        
+        self.irACValueSqSum = self.irACValueSqSum + (self.dcFilterIR.result * self.dcFilterIR.result)
+        self.redACValueSqSum = self.redACValueSqSum  + (self.dcFilterRed.result * self.dcFilterRed.result)
+        self.samplesRecorded = self.samplesRecorded + 1
+
+        if( self.detectPulse( self.lpbFilterIR.result ) and self.samplesRecorded > 0 ):
+            result.pulseDetected = True
+            self.pulsesDetected = self.pulsesDetected + 1
+
+            ratioRMS = math.log( math.sqrt(self.redACValueSqSum / self.samplesRecorded), 10 ) / math.log( math.sqrt(self.irACValueSqSum / self.samplesRecorded), 10 )
+
+            if(self.debug):
+                print("RMS Ratio: {}".format(str(ratioRMS)))
+
+            # //This is my adjusted standard model, so it shows 0.89 as 94% saturation. It is probably far from correct, requires proper empircal calibration
+            self.currentSaO2Value = 110.0 - 18.0 * ratioRMS
+            result.SaO2 = self.currentSaO2Value
+            
+            if( self.pulsesDetected % self.RESET_SPO2_EVERY_N_PULSES == 0):
+                self.irACValueSqSum = 0
+                self.redACValueSqSum = 0
+                self.samplesRecorded = 0
+        else:
+            if(self.debug):
+                print("Not entring detectPulse!!!!")
+
+
+        return result
+
+
+    def dcRemoval(self, x, prev_w, alpha):
+        filtered = dcFilter_t()
+        filtered.w = x + alpha * prev_w
+        filtered.result = filtered.w - prev_w
+
+        return filtered
+
+
+    def meanDiff(self, M, filterValues):
+        avg = 0
+
+        filterValues.sum = filterValues.sum - filterValues.values[filterValues.index]
+        filterValues.values[filterValues.index] = M
+        filterValues.sum = filterValues.sum + filterValues.values[filterValues.index]
+
+        filterValues.index = filterValues.index + 1
+        filterValues.index = filterValues.index % self.MEAN_FILTER_SIZE
+
+        if(filterValues.count < self.MEAN_FILTER_SIZE):
+            filterValues.count = filterValues.count + 1
+
+        avg = filterValues.sum / filterValues.count
+        
+        return (avg - M, filterValues)
+
+    def lowPassButterworthFilter(self, x, filterResult):
+        filterResult.v[0] = filterResult.v[1]
+
+        # Fs = 100Hz and Fc = 10Hz
+        filterResult.v[1] = (2.452372752527856026e-1 * x) + (0.50952544949442879485 * filterResult.v[0])
+
+        # Fs = 100Hz and Fc = 4Hz
+        # filterResult.v[1] = (1.367287359973195227e-1 * x) + (0.72654252800536101020 * filterResult.v[0]) #Very precise butterworth filter 
+
+        filterResult.result = filterResult.v[0] + filterResult.v[1]
+
+        return filterResult
+
+
+    def detectPulse(self, sensor_value):
+        if(self.debug):
+            print("Inside detectPulse with {} current pulse state {}".format(sensor_value, self.currentPulseDetectorState))
+
+        # why this variables are static
+        prev_sensor_value = 0
+        values_went_down = 0
+        currentBeat = 0
+        lastBeat = 0
+
+        if(sensor_value > self.PULSE_MAX_THRESHOLD):
+            self.currentPulseDetectorState = self.PULSE_IDLE
+            prev_sensor_value = 0
+            lastBeat = 0
+            currentBeat = 0
+            values_went_down = 0
+            lastBeatThreshold = 0
+            return False
+
+
+        if(self.currentPulseDetectorState == self.PULSE_IDLE):
+            if(sensor_value >= self.PULSE_MIN_THRESHOLD): 
+                self.currentPulseDetectorState = self.PULSE_TRACE_UP
+                values_went_down = 0
+
+        elif(self.currentPulseDetectorState == self.PULSE_TRACE_UP):
+            if(sensor_value > prev_sensor_value):
+                currentBeat = lambda: int(round(time.time() * 1000))
+                self.lastBeatThreshold = sensor_value
+            else:
+                if(self.debug == True): 
+                    print("Peak reached: {} and {}".format(sensor_value, prev_sensor_value))
+
+                beatDuration = currentBeat - lastBeat
+                lastBeat = currentBeat
+
+                rawBPM = 0
+                if(beatDuration > 0):
+                    rawBPM = 60000.0 / beatDuration
+                
+                if(self.debug == True): 
+                    print("Raw BRM {}".format(rawBPM))
+
+                # //This method sometimes glitches, it's better to go through whole moving average everytime
+                # //IT's a neat idea to optimize the amount of work for moving avg. but while placing, removing finger it can screw up
+                # //valuesBPMSum -= valuesBPM[bpmIndex];
+                # //valuesBPM[bpmIndex] = rawBPM;
+                # //valuesBPMSum += valuesBPM[bpmIndex];
+
+                self.valuesBPM[self.bpmIndex] = rawBPM
+                self.valuesBPMSum = 0
+                i = 0
+                while(i < self.PULSE_BPM_SAMPLE_SIZE):
+                    self.valuesBPMSum = self.valuesBPMSum + self.valuesBPM[i]
+                    i = i + 1
+
+                if(self.debug == True): 
+                    print("CurrentMoving Avg: {}".format(self.valuesBPM))
+
+                self.bpmIndex = self.bpmIndex + 1
+                self.bpmIndex = self.bpmIndex % self.PULSE_BPM_SAMPLE_SIZE
+
+                if(self.valuesBPMCount < self.PULSE_BPM_SAMPLE_SIZE):
+                    self.valuesBPMCount = self.valuesBPMCount + 1
+
+                self.currentBPM = self.valuesBPMSum / self.valuesBPMCount
+                if(self.debug == True): 
+                    print("AVg. BPM: {}".format(self.currentBPM))
+
+                self.currentPulseDetectorState = self.PULSE_TRACE_DOWN
+
+                return True
+
+        elif(self.currentPulseDetectorState == self.PULSE_TRACE_DOWN):
+            if(sensor_value < prev_sensor_value):
+                values_went_down = values_went_down + 1
+
+            if(sensor_value < self.PULSE_MIN_THRESHOLD):
+                self.currentPulseDetectorState = self.PULSE_IDLE;
+
+        prev_sensor_value = sensor_value;
+
+        return False;
 
 
 
--- test_max30100.py
+++ test_max30100.py
@@ -6,41 +6,26 @@ import max30100
 
 def main():
 	try:
-		mx30 = max30100.MAX30100()
-		# mx30.set_mode(max30100.MODE_SPO2)
-
-		# mx30.reset()
-		# time.sleep(1)
-		# mx30.startup()
-		# time.sleep(1)
-
+		mx30 = max30100.MAX30100(
+					debug=False,
+					led_current_red=33.8,
+                	led_current_ir=33.8,
+				)
+		mx30.set_mode(max30100.MODE_SPO2)
 
 		i = 0
-		while i < 30:
-			# mx30.set_mode(max30100.MODE_HR)
+		while i < 60:
 			# mx30.read_sensor()
-			# # The latest values are now available via .ir and .red
-			# print("HeartRate sensor .ir: {} and .red: {}".format(mx30.ir, mx30.red))
-
-			# time.sleep(2)
-			# mx30 = max30100.MAX30100()
-			mx30.reinit()
-			mx30.set_mode(max30100.MODE_SPO2)
-			# time.sleep(1)
-
-			mx30.read_sensor()
+			mx30.update()
 			# The latest values are now available via .ir and .red
-			print("HRate sensor .ir: {} and SpO2 .red: {}".format(mx30.ir, mx30.red))
-
-
-			# temp = mx30.get_temperature()
-			# print("Temperature {}\n".format(temp))
-			mx30.reset()
-			time.sleep(1)
-
+			# print("HRate sensor .ir: {} and SpO2 .red: {}".format(mx30.ir, mx30.red))
+			# print("\n")
+			time.sleep(0.010)
 			i = i + 1
+
+		print("Current BPM {}".format(mx30.currentBPM))
+		print("values of BPM {}".format(mx30.valuesBPM))
 		mx30.reset()
-		# mx30.shutdown()
 
 
 	except Exception as e:
